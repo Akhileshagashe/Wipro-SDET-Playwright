ea26346eb553f51320be429348efd6e72355b7e0
Akhilesh Agashe <akhilesh.agashe05@gmail.com>
2026-04-30 16:10:55 +0530

    arrays and functions

--- Day1/first.js
+++ Day1/first.js
@@ -31,4 +31,21 @@ console.log(c.toUpperCase());
 
 
 
-let arr = [1, "Aaryan", 3, true, 5];
+let arr = [1, "Akhilesh", 3, false, 5];
+let arr2 = [1, 2, 3, 4, 5];
+
+for (let i = 0; i < arr2.length; i++){
+    console.log("Element at index after * 2 ", i, ":", arr2[i] * 2);
+}
+let count = arr2.length - 1;
+while (count >= 0) {
+    console.log("Element at index after * 2 ", count, ":", arr2[count] * 2);
+    count--;
+}
+
+arr2.forEach(function(x) {
+    console.log("Element after * 2 ", x, ":", x * 5);
+});
+arr2.map(function(x) {
+    console.log("Element after * 2 ", x, ":", x * 10);
+});
