a33aa39ac840968ba1ecb5aca499319fc67cad11
Akhilesh Agashe <akhilesh.agashe05@gmail.com>
2026-04-30 15:07:55 +0530

    Arithmatic Operations, Relational operators, String operations, Logical operators

---
+++ Day1/first.js
@@ -0,0 +1,30 @@
+let a = 5;
+let b = 2;
+
+// Arithmatic Operations
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+// Relational operators
+
+console.log(a>b);
+console.log(a<b);
+console.log(a>=b);
+console.log(a<=b);
+console.log(a==b);
+console.log(a!=b);
+
+//Logical operators
+
+console.log(a>1 && b>1);
+console.log(a>1 || b<0);
+
+// String operations
+
+let c = "Akhilesh";
+console.log(c.toLowerCase());
+console.log(c.toUpperCase());
