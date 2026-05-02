820152a7d807f3e0312663fa761be7a75468aaf9
Akhilesh Agashe <akhilesh.agashe05@gmail.com>
2026-05-02 16:29:11 +0530

    Reduce method

--- Day2/index.js
+++ Day2/index.js
@@ -23,3 +23,20 @@ console.log("Sum of 5 and 10:",sum(5,10));
 let arr = [1,2,"Akhilesh",4,5];
 
 arr.filter(x => typeof x === "number").map(x => x*5).forEach(x => console.log(x)); 
+
+
+
+// Reduce method examples
+
+const prices = [10, 20, 30, 40, 50];
+const total = prices.reduce((acc, price) => acc + price, 0);
+console.log("Total price:", total);
+
+
+
+const colors = ["red", "green", "blue", "red", "red", "green", "purple"];
+const colorCount = colors.reduce((acc, color) => {
+    acc[color] = (acc[color] || 0) + 1;
+    return acc;
+}, {});
+console.log("Color count:", colorCount);
